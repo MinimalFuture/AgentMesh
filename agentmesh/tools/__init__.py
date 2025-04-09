0fa29bc0f1fbe511efe93d1f088eb380ade951d4
zhayujie <yjzha1996@163.com>
2025-04-09 21:11:18 +0800

    fix: remove unimplemented tools

--- agentmesh/tools/__init__.py
+++ agentmesh/tools/__init__.py
@@ -6,7 +6,6 @@ from agentmesh.tools.tool_manager import ToolManager
 from agentmesh.tools.google_search.google_search import GoogleSearch
 from agentmesh.tools.calculator.calculator import Calculator
 from agentmesh.tools.current_time.current_time import CurrentTime
-from agentmesh.tools.browser.browser_tool import BrowserTool
 
 # Export all tools
 __all__ = [
@@ -14,6 +13,5 @@ __all__ = [
     'ToolManager',
     'GoogleSearch',
     'Calculator',
-    'CurrentTime',
-    'BrowserTool'
+    'CurrentTime'
 ]
